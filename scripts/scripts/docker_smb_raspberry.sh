b75ab3456679f5b2faafe9ae76818a01e1220e76
johnfb64 <jhonn.ballen@gmail.com>
2022-11-19 21:34:18 -0500

    Se actualiza rwx en docker smb

--- scripts/scripts/docker_smb_raspberry.sh
+++ scripts/scripts/docker_smb_raspberry.sh
@@ -1,14 +1,10 @@
+#!/bin/bash
 docker run -it -e TZ=America/Bogota \
--v /root/shared/Compartida1:/Compartida1 \
--v /root/shared/Compartida2:/Compartida2 \
--v /root/shared/Compartida3:/Compartida3 \
+-v /root/shared/Compartida1:/Compartida1:rw \
+-v /root/shared/Compartida2:/Compartida2:rw \
+-v /root/shared/Compartida3:/Compartida3:rw \
 -p 139:139 -p 445:445 -d dperson/samba -p -u "john;pepe123" \
--s "Compartida1;/Compartida1;yes;yes" \
--s "Compartida2;/Compartida2;yes;yes" \
--s "Compartida3;/Compartida3;yes;yes" \
+-s "Compartida1;/Compartida1;yes;no" \
+-s "Compartida2;/Compartida2;yes;no" \
+-s "Compartida3;/Compartida3;yes;no" \
 -g "ntlm auth = yes"
-
-
-
-
-
